3f2a4eac838d9c6a667740dcd60b4cb5737748ba
GeorgeMC2610 <giorgosseimenis@gmail.com>
2022-02-19 16:25:53 +0200

    βάλαμε ποσοστά για τον σκύλο και τη γάτα

--- Άσκηση 3/Smart-Home/Cat.cs
+++ Άσκηση 3/Smart-Home/Cat.cs
@@ -8,11 +8,23 @@ namespace Smart_Home
 {
     class Cat : Animal
     {
+        //Τυχαία ονόματα γάτας.
+        private static string[] Names = { "Jack", "Loki", "Jasper", "Garfield", "Simba", "Soozie", "Ellie", "Raymond", "Lucy", "Kitty"};
+
+        //Όπως και στον σκύλο, έτσι και στην γάτα το όνομα είναι τυχαίο.
         public Cat() : base()
-        {    
+        {
+            name = Names[random.Next(0, Names.Length)];
         }
-
-        override 
-
+        
+        /// <summary>
+        /// Μία γάτα είναι λιγότερο πιθανό να κάνει ζημιά από έναν σκύλο. Οπότε, κάθε φορά που καλείται η μέθοδος, υπάρχει 50% πιθανότητα να ανεβεί το ποσοστό δραστηριότητάς της.
+        /// </summary>
+        public override void Awaken()
+        {
+            Asleep = false;
+            if (random.Next(0, 2) == 0)
+                ActivityPercentage++;
+        }
     }
 }
--- Άσκηση 3/Smart-Home/Dog.cs
+++ Άσκηση 3/Smart-Home/Dog.cs
@@ -8,14 +8,18 @@ namespace Smart_Home
 {
     class Dog : Animal
     {
+        //Ο σκύλος και η γάτα θα έχουν ορισμένα ονόματα, τα οποία θα επιλέγουμε τυχαία.
         private static string[] Names = { "Charlie", "Bella", "Max", "Buddy", "Luna", "Coco", "Ruby", "Milo", "Archie" };
 
+        //To base() σημαίνει ότι καλούμε και τον constructor της Animal.
         public Dog() : base()
         {
             name = Names[random.Next(0, Names.Length)];
         }
 
-        //Ο σκύλος έχει πιθανότητα 50% να είναι πιο ζωηρός από την γάτα
+        /// <summary>
+        /// Ο σκύλος, έχει περισσότερες πιθανότητες να κάνει ζημιά σε σχέση με μία γάτα. Οπότε, ο σκύλος έχει 50% πιθανότητα να του ανέβει η δραστηριότητα κάτα 2 μονάδες και 50% να του ανεβεί κατά μία μονάδα.
+        /// </summary>
         public override void Awaken()
         {
             Asleep = false;
