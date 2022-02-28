06b754a452c44fbff2b05029c91f8cf3f79b4193
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2022-02-28 14:35:32 +0200

    αλλαγές στο πρόγραμμα ημέρας

--- Άσκηση 3/Smart-Home/TimeSchedule.Designer.cs
+++ Άσκηση 3/Smart-Home/TimeSchedule.Designer.cs
@@ -31,6 +31,8 @@ namespace Smart_Home
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeSchedule));
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -40,6 +42,35 @@ namespace Smart_Home
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 0;
             // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.Color.Maroon;
+            this.button20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button20.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button20.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button20.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button20.Location = new System.Drawing.Point(490, 65);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(214, 31);
+            this.button20.TabIndex = 38;
+            this.button20.Text = "Διαθέσιμα Παπούτσια";
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(293, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 31);
+            this.button1.TabIndex = 39;
+            this.button1.Text = "Διαθέσιμα Παπούτσια";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // TimeSchedule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -47,11 +78,14 @@ namespace Smart_Home
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button20);
             this.Controls.Add(this.dateTimePicker1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TimeSchedule";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Διαχείριση Χρονοπρογράμματος";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TimeSchedule_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -59,5 +93,7 @@ namespace Smart_Home
         #endregion
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Άσκηση 3/Smart-Home/TimeSchedule.cs
+++ Άσκηση 3/Smart-Home/TimeSchedule.cs
@@ -12,9 +12,93 @@ namespace Smart_Home
 {
     public partial class TimeSchedule : Form
     {
+
+        // παπούτσια που μπορεί να έχει ένας άνδρας και μία γυναίκα (δύο ξεχωριστοί πίνακες)
+        string[] shoes_gen = {"Αθλητικά", "Μοκασίνια", "Sneakers", "Παπούτσια ποδοσφαίρου", "Σκαρπίνια"};
+        
+        // η λίστα αυτή εμπεριέχει παπούτσια που υπάρχουν μέσα στην παπουτσοθήκη (επιλέγονται κάθε φορά τυχαία)
+        List<String> shoes = new List<string>();
+
+        bool T; // διακόπτης
+
         public TimeSchedule()
         {
             InitializeComponent();
+            T = true; 
+        }
+
+        // ο χρήστης πατώντας αυτό το κουμπί, μπορεί να δει τα διαθέσιμα παπούτσια που βρίσκονται
+        // στην παπουτσοθήκη αυτή τη στιγμή
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if (T)
+            {
+                MessageBox.Show("in");
+                // προσθήκη τυχαίων παπουτσιών στη λίστα shoes (γίνεταί μία φορά, ενόσω εκτελείται το πρόγραμμα)
+
+                Random rnd = new Random(); // τυχαία επιλογή επαναλήψεων for loop
+                Random random_shoe = new Random(); // τυχαία επιλογή παπουτσιού από τον πίνακα shoes_gen (επιτρέπονται και διπλότυπα)
+
+                int j = rnd.Next(5);
+                for (int i = 0; i < j; i++)
+                {
+                    shoes.Add(shoes_gen[random_shoe.Next(5)]);
+                }
+
+                T = false;
+            }
+
+                if (shoes.Count() == 0)
+            {
+                MessageBox.Show("Η παπουτσοθήκη σας είναι άδεια αυτή τη στιγμή.");
+            }
+            else
+            {
+                // μετρητές παπουτσιών ανά είδος
+                int count_mokasinia = 0;
+                int count_sneakers = 0;
+                int count_athlitika = 0;
+                int count_skarpinia = 0;
+                int count_football = 0;
+
+                for (int i = 0; i < shoes.Count(); i++)
+                {
+                    if (shoes[i].Equals("Aθλητικά"))
+                    {
+                        count_athlitika += 1;
+                    }
+                    else if (shoes[i].Equals("Μοκασίνια"))
+                    {
+                        count_mokasinia += 1;
+                    }
+                    else if (shoes[i].Equals("Sneakers"))
+                    {
+                        count_sneakers += 1;
+                    }
+                    else if (shoes[i].Equals("Παπούτσια ποδοσφαίρου"))
+                    {
+                        count_football += 1;
+                    }
+                    else if (shoes[i].Equals("Σκαρπίνια"))
+                    {
+                        count_skarpinia += 1;
+                    }
+                }
+
+                // εμφάνιση μηνύματος προς τον χρήστη
+                MessageBox.Show("H παπουτσοθήκη σας περιέχει τα ακόλουθα ζευγάρια παπουτσιών: " +
+                    "Sneakers --> " + count_sneakers.ToString() + " , " + "Μοκασίνια --> " + count_mokasinia.ToString() +
+                    " , " + "Σκαρπίνια --> " + count_skarpinia.ToString() + " , " + "Παπούτσια Ποδοσφαίρου --> " +
+                    count_football.ToString() + " , " + "Αθλητικά --> " + count_athlitika.ToString() + ".");
+            }
+        }
+
+        // ενέργειες που γίνονται όταν ο χρήστης πατάει το κουμπί Χ (κλείσιμο φόρμας)
+        private void TimeSchedule_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // η φόρμα κρύβεται όταν ο χρήστης πατήσει το X. Με αυτόν τον τρόπο, οι αλλαγές που κάνει στην φόρμα δεν χάνονται άπαξ και την ξαναανοίξει (ενόσω εκτελείται το πρόγραμμα φυσικά)
+            this.Hide();
+            e.Cancel = true;
         }
     }
 }
